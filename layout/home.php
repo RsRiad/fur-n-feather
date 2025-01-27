2a7af30aea2c1b675e9b59d87c2d4d2a5479bcf7
RsRiad <rsriad00@gmail.com>
2025-01-28 01:00:36 +0600

    1st

---
+++ layout/home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riad's Portfolio</title>
+    <link rel="stylesheet" href="./css/home.css">
+ 
+    <link rel="preconnect" href="https://f...content-available-to-author-only...s.com">
+    <link rel="preconnect" href="https://f...content-available-to-author-only...c.com" crossorigin>
+    <link href="https://f...content-available-to-author-only...s.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+ 
+    <link rel="preconnect" href="https://f...content-available-to-author-only...s.com">
+<link rel="preconnect" href="https://f...content-available-to-author-only...c.com" crossorigin>
+<link href="https://f...content-available-to-author-only...s.com/css2?family=Mogra&display=swap" rel="stylesheet">
+ 
+<link rel="preconnect" href="https://f...content-available-to-author-only...s.com">
+<link rel="preconnect" href="https://f...content-available-to-author-only...c.com" crossorigin>
+<link href="https://f...content-available-to-author-only...s.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+</head>
+<body class="open-sans-normal">
+    <header class="header secondary-bg">
+        <nav class="">
+            <h3 class="nav-title">Fur<span class="text-primary"> n' </span>Feather</h3>
+            <ul>
+ 
+                <li><a class="dark-2" href="">Home</a></li>
+                <li ><a class="dark-2" href="">About</a></li>
+                <li ><a class="dark-2" href="">Our Pets</a></li>
+                <li ><a class="dark-2" href="">Contact</a></li>
+                <li><a href=""><button class="btn-shop">Shop</button></a></li>
+            </ul>
+        </nav>
+        <div class="banner-description">
+                    <p id="p1">A place for </p>
+                        <p id="p2"> you and</p> 
+                            <p id="p3" >your pet...</p>
+        </div>
+        <div class="banner">
+            <div class="banner-content">
+ 
+                <div class="msg"><p class="msg1">Adopt<br><span class="msg2"> Don't Shop</p></div>
+                <button class="btn-primary1">Log In</button>
+                <button class="btn-primary2">Sign Up</button>
+            </div>
+ 
+        </div>
+    </header>
+    <main>
+ 
+    </main>
+    <footer>
+ 
+    </footer>
+</body>
+</html>
---
+++ layout/shop.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organic Store</title>
+    <link rel="shortcut icon" href="./images/logo.svg" type="image/x-icon">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- External CSS -->
+    <link rel="stylesheet" href="./css/shop.css">
+
+</head>
+
+<body class="font-poppins">
+    <!-- Header -->
+    <header>
+        <section class="container">
+            <nav class="navbar">
+                <div class="navbar-start">
+                    <a class="logo">
+                        <img id="logo" src="./assets/logo.png" class="logo-img">
+                    </a>
+                </div>
+                <div class="navbar-center">
+                    <ul class="menu">
+                        <li><a href="#" class="menu-item">Home</a></li>
+                        <li><a href="#" class="menu-item">Pets</a></li>
+                        <li><a href="#" class="menu-item">Accessories</a></li>
+                        <li><a href="#" class="menu-item">About</a></li>
+                        <li><a href="#" class="menu-item">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="navbar-end">
+                    <div class="cart">
+                        <span class="cart-amount">$1000.00</span>
+                        <!-- <i class="fa-solid fa-cart-shopping"></i> -->
+                    </div>
+                    <div class="user-icon">
+                        <!-- <a><i class="fa-solid fa-user"></i></a> -->
+                    </div>
+                </div>
+            </nav>
+        </section>
+
+        <section class="hero">
+            <div class="hero-content">
+                <div class="hero-image">
+                    <img src="./assets/banner1.png" alt="">
+                </div>
+                <div class="hero-text">
+                    <h1>Meet Your Match</h1>
+                    <h2>Find your perfect companion! Explore a wide range of <span class="title-co">Adorable Pets</span> ready for adoption.</h2>
+                    <p>Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn">Get Started <i class="fa-solid fa-cart-shopping"></i></button>
+                </div>
+            </div>
+        </section>
+    </header>
+
+    <!-- Main Content -->
+    <main>
+        <section class="features">
+            <div class="feature-card">
+                <img src="./assets/logo1.png" alt="">
+                <h4>Free Shipping</h4>
+                <p>Above $5 Only</p>
+            </div>
+            <div class="feature-card">
+                <img src="./assets/logo2.png" alt="">
+                <h4>Certified Quality</h4>
+                <p>Best assurance</p>
+            </div>
+            <div class="feature-card">
+                <img src="./assets/logo3.png" alt="">
+                <h4>Save More</h4>
+                <p>Exclusive deals</p>
+            </div>
+            <!-- <div class="feature-card">
+                <i class="fa-solid fa-recycle feature-icon"></i>
+                <h4>Sustainable</h4>
+                <p>Eco-friendly products</p>
+            </div> -->
+        </section>
+
+        <section class="about">
+            <div class="about-text">
+                <h3>About Us</h3>
+                <h2>Tell Website Visitors Who You Are</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod consequatur soluta at reprehenderit
+                    sunt animi natus quibusdam voluptatum, consectetur exercitationem nobis ab in esse nisi mollitia
+                    ipsa fuga odio autem.</p>
+                <button class="btn">Find Out More</button>
+            </div>
+            <div class="about-image">
+                <img class="banner2-pic" src="./assets/banner2.jpg" alt="About Us">
+            </div>
+        </section>
+
+        <section class="cta">
+            <div class="cta-overlay">
+                <div class="cta-content">
+                    <h1>Insert a Call to Action</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi asperiores soluta autem quia
+                        possimus eos facilis!</p>
+                    <button class="btn btn-outline">Get Started</button>
+                </div>
+            </div>
+        </section>
+        <h1>Accessories</h1>
+        <div class="product-grid">
+            <!-- Product Card 1 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p1.webp" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 2 -->
+            <div class="product-card">
+                <div><img class="p-img" src="./assets/product/p2.png" alt="Assorted Coffee"></div>
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 3 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p3.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 4 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p4.webp" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p5.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 2 -->
+            <div class="product-card">
+                <div><img class="p-img" src="./assets/product/p6.jpg" alt="Assorted Coffee"></div>
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 3 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p7.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 4 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p8.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p9.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 6 -->
+            <div class="product-card">
+                <div><img class="p-img" src="./assets/product/p10.jpg" alt="Assorted Coffee"></div>
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 7 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p11.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Product Card 8 -->
+            <div class="product-card">
+                <img class="p-img" src="./assets/product/p12.jpg" alt="Assorted Coffee">
+                <h3>Assorted Coffee</h3>
+                <p class="price">$35.00</p>
+                <button class="buy-now">Buy Now</button>
+            </div>
+            <!-- Add more product cards as needed -->
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer">
+            <aside>
+                <img src="./images/logo.svg" alt="Logo">
+                <p>Providing reliable tech since 1992</p>
+            </aside>
+            <nav>
+                <h6>Services</h6>
+                <a href="#">Branding</a>
+                <a href="#">Design</a>
+                <a href="#">Marketing</a>
+                <a href="#">Advertisement</a>
+            </nav>
+            <nav>
+                <h6>Company</h6>
+                <a href="#">About us</a>
+                <a href="#">Contact</a>
+                <a href="#">Jobs</a>
+                <a href="#">Press kit</a>
+            </nav>
+            <nav>
+                <h6>Legal</h6>
+                <a href="#">Terms of use</a>
+                <a href="#">Privacy policy</a>
+                <a href="#">Cookie policy</a>
+            </nav>
+        </section>
+    </footer>
+</body>
+
+</html>
